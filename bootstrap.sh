7ae600ffd127e842a542a55eab7e07dd8c082534
Stefan Armbruster <stefan@armbruster-it.de>
2014-02-28 17:05:20 +0100

    workaround for /db/manage
    Neo4j returns fixed URL using http://localhost:7474 when HTTP GET is
    sent to /db/manage. That URL is intercepted and patched using
    mod_substitute

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,13 +6,13 @@ echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.
 
 apt-get update
 apt-get upgrade
-apt-get install -y apache2 neo4j
+apt-get install -y apache2 neo4j 
 
 # listen on all interfaces 
 #rm /etc/neo4j/neo4j-server.properties; ln -s /vagrant/etc/neo4j/neo4j-server.properties /etc/neo4j
 #/etc/init.d/neo4j-service restart
 
-a2enmod proxy_http
+a2enmod proxy_http substitute
 a2enmod ssl
 a2ensite default-ssl
 rm /etc/apache2/sites-available/default; ln -s /vagrant/etc/apache2/sites-available/default /etc/apache2/sites-available/
